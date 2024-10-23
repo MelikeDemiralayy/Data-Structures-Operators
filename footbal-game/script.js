b48b76c552ef3e32133662842bd18be8b57f545a
melike demiralay <melikedemiralay3215@gmail.com>
2024-10-23 12:04:25 +0300

    Utilized Object.entries() for iterating through odds

--- footbal-game/script.js
+++ footbal-game/script.js
@@ -174,3 +174,92 @@ printGoals(...game.scored);
 //cözüm-7: hangisinin kazanma ihtimali daha yüksek
 team1 < team2 && console.log("Team 1 is more likely team win");
 team1 > team2 && console.log("Team 2 is more likely team win");
+
+
+
+
+/* 
+coding challenge-2
+
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+*/
+const game1 = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+
+//1.cözüm:
+for(const [i,player] of game1.scored.entries()){ // entries() methodu:bir veri yapısının hem indexini hemde değerini ayn anda döndürür.
+  console.log(`Goal ${i+1}:${player}`)
+}
+
+//cözüm-2:
+const odds = Object.values(game1.odds);
+let average = 0;
+
+for (const odd of odds) {
+  average += odd; 
+  console.log(`Toplam şu anda: ${average}`);  
+}
+
+average /= odds.length;
+console.log(`Ortalama: ${average}`); 
+
+
+//cözüm-3;
+for(const [team, odd] of Object.entries(game1.odds)){
+  const teamStr = team === 'x'? 'draw': `victory ${game1[team]}`
+console.log(`Odd of  ${teamStr} ${odd}`)
+}
